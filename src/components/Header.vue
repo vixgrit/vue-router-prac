bd06947c732365e1ac1ae447e4a7ddfba5aef0fc
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-16 01:49:00 -0400

    navigate with router links

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <ul class="nav nav-pills">
+  <li role="presentation"><router-link to="/">Home</router-link></li>
+  <li role="presentation"><router-link to="/user">User</router-link></li>
+</ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
