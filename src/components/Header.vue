ca12f0577e2fa83d75648fab772598652d07e895
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-16 02:04:41 -0400

    styling active links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <ul class="nav nav-pills">
-  <li role="presentation"><router-link to="/">Home</router-link></li>
-  <li role="presentation"><router-link to="/user">User</router-link></li>
+  <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+  <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
 </ul>
 </template>
 
